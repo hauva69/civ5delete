50c3909f1a8b63322eb64fab2d18dc5c7418eecf
Ari Makela <ari.makela@iki.fi>
2018-08-08 16:18:55 +0300

    rm => rm -f

---
+++ civ5delete.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+DATA_DIR="${HOME}/Library/Containers/com.aspyr.civ5campaign/Data/Library/Application Support/Civilization V Campaign Edition/Saves/single"
+
+rm -f "$DATA_DIR/"*.Civ5Save "$DATA_DIR/auto/"*.Civ5Save
